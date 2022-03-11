f00736861bb362bb3865b991d2b7e86ef94e2b4b
anhtay <tayphongtran@gmail.com>
2022-03-11 14:19:11 +0700

    test

---
+++ Student.js
@@ -0,0 +1,25 @@
+class Student {
+    constructor(id,name, age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+    setId(id){
+        this.id = id
+    }
+    getId(){
+        return this.id
+    }
+    setName(name){
+        this.name = name
+    }
+    getName(){
+        return this.name
+    }
+    setAge(age){
+        this.age = age
+    }
+    getAge(){
+        return this.age
+    }
+}
